d690e5fc800bc6bacd54228f6744199542b329c4
Antonio Sarro <tech@antoniosarro.dev>
2024-09-08 21:47:45 +0200

    feat: fixed rofi uptime script

--- home/scripts/rofi/default.nix
+++ home/scripts/rofi/default.nix
@@ -11,8 +11,23 @@
     fi
   '';
   powermenu = pkgs.writeShellScriptBin "powermenu" ''
-    # CMDs
-    uptime=$(uptime -p | sed -e 's/up //g')
+    # Uptime
+    s=$(< /proc/uptime)
+    d="$((s / 60 / 60 / 24)) days"
+    h="$((s / 60 / 60 % 24)) hours"
+    m="$((s / 60 % 60)) minutes"
+    # Remove plural if < 2.
+    ((''${d/ *} == 1)) && d=''${d/s}
+    ((''${h/ *} == 1)) && h=''${h/s}
+    ((''${m/ *} == 1)) && m=''${m/s}
+    # Hide empty fields.
+    ((''${d/ *} == 0)) && unset d
+    ((''${h/ *} == 0)) && unset h
+    ((''${m/ *} == 0)) && unset m
+    uptime=''${d:+$d, }''${h:+$h, }$m
+    uptime=''${uptime%', '}
+    uptime=''${uptime:-$s seconds}
+
 
     # Options
     shutdown='󰐥'
