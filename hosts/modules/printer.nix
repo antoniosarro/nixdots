1b91511b3327f097619ea5672edca3ac2c97c6ad
Antonio Sarro <tech@antoniosarro.dev>
2024-09-09 00:08:48 +0200

    fix: printer name

--- hosts/modules/printer.nix
+++ hosts/modules/printer.nix
@@ -8,7 +8,7 @@
   hardware.printers = {
     ensurePrinters = [
       {
-        name = "Epson XP-302 303 305 306";
+        name = "Epson_XP-302_303_305_306";
         location = "Stampante";
         deviceUri = "socket://10.10.40.16:9100";
         model = "Epson XP-302 303 305 306 Series, Epson Inkjet Printer Driver (ESC/P-R) for Linux";
@@ -17,6 +17,6 @@
         };
       }
     ];
-    ensureDefaultPrinter = "Epson XP-302 303 305 306";
+    ensureDefaultPrinter = "Epson_XP-302_303_305_306";
   };
 }
